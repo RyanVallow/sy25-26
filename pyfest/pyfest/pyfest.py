319591f81e206b0a3ef42f51db0572aca6750f91
RyanVallow <rcval972@learner.dvusd.org>
2026-01-13 13:04:17 -0700

    Add files via upload

---
+++ pyfest/pyfest/pyfest.py
@@ -0,0 +1,10 @@
+print("\n---py-fest 2026 stage manager ---")
+print("1. View Lineup & Total Time")
+print("2. Add a New Band")
+print("3. Move First Band to End (Late Arrival)")
+print("4. Remove a Band by Name")
+print("5. Move band to specific position") 
+print("6. Exit")
+
+choice = input("Choose an option (1-6): ")
+
